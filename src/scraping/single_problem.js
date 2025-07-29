d211a6704b0b35b9173d34d750f1188a76d08ac3
Philippe Fanaro <philippefanaro@gmail.com>
2025-07-29 13:31:28 +0900

    able to get the data from the page and transform it into a working SGF string

--- src/scraping/single_problem.js
+++ src/scraping/single_problem.js
@@ -1,89 +1,55 @@
 const puppeteer = require("puppeteer")
 
-/**
- * From [this answer](https://stackoverflow.com/a/79717008/4756173)
- */
-function parseGamePos(config) {
-  if (Array.isArray(config.content)) return config.content
-
-  const i = config.ru + 1
-  const key = `101${i}${i}${i}`
-  const bytes = atob(String(config.content).replace(/[\t\n\f\r ]+/g, ""))
-  const output = []
-
-  let keyIndex = 0
-  for (let n = 0; n < i.length; ++n) {
-    output.push(
-      String.fromCharCode(bytes.charCodeAt(n) ^ key.charCodeAt(keyIndex))
-    )
-
-    keyIndex = (keyIndex + 1) % key.length
-  }
-
-  return JSON.parse(output.join(""))
-}
-
 const sampleProblemUrl = "https://www.101weiqi.com/q/17773/"
 const weiqi101signup = "https://www.101weiqi.com/login/"
 
 const dummyAccount = "comoti7227@atebin.com"
 
-async function fetchProblem() {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+let browser
+let page
+
+async function login() {
+  browser = await puppeteer.launch()
+  page = await browser.newPage()
+
+  // Login
 
   await page.goto(weiqi101signup)
-  
+
   await page.type(`input[x-model="loginName"]`, dummyAccount)
   await page.type(`input[x-model="loginPassword"]`, dummyAccount)
 
   await page.click(`.login-button.active`)
-  
+
   await page.waitForNavigation()
-
-  await page.goto(sampleProblemUrl)
-  const bodyHtml = await page.evaluate(() => document.body.innerHTML);
-  console.log(bodyHtml);
-
-  
-  // const req = await fetch(url)
-  // const body = await req.text()
-
-  await browser.close();
-
-  // console.log(body)
-  // const matches = body.match(/script/)
-  // console.log(matches)
-  // const idx = body.indexOf('qqdata')
-  // if (idx !== -1) {
-  //   console.log(body.slice(idx - 100, idx + 500))
-  // } else {
-  //   console.log("qqdata not found in HTML at all")
-  // }
-
-  // // Try a more flexible regex (supports var/let/const, optional semicolon)
-  // const match = body.match(/(?:var|let|const)\s+qqdata\s*=\s*({[\s\S]*?})\s*;/)
-  // if (match) {
-  //   try {
-  //     const qqdata = JSON.parse(match[1])
-  //     console.log(qqdata)
-  //   } catch (e) {
-  //     console.error("Failed to parse qqdata:", e)
-  //     console.log("Extracted string:", match[1])
-  //   }
-  // } else {
-  //   console.log("qqdata not found with regex")
-  // }
-
-  // const dom = new JSDOM(body, {
-  //   runScripts: "dangerously",
-  //   timeout: 10_000
-  // })
-  // const window = dom.window
-  // console.log(window)
-  // console.log(window.qqdata)
 }
 
-// console.log("Fetching game data...")
+function toSGFCoords(data) {
+  const blackCoords = data.content[0]
+  const whiteCoords = data.content[1]
+  const sgfPrefix = ";GM[1]FF[4]CA[UTF-8]SZ[19]"
+
+  const blackStones = blackCoords.map((coord) => `[${coord}]`).join("")
+  const whiteStones = whiteCoords.map((coord) => `[${coord}]`).join("")
+
+  const sgf = `(${sgfPrefix}AB${blackStones}AW${whiteStones})`
+  
+  return sgf
+}
+
+async function fetchProblem() {
+  await login()
+
+  // Go to the Problem Page
+
+  await page.goto(sampleProblemUrl)
+
+  // Getting the Data into an SGF
+
+  const qqdata = await page.evaluate(() => window.qqdata)
+  console.log(toSGFCoords(qqdata))
+
+  await browser.close()
+}
 
 fetchProblem()
