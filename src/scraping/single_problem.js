0ded42baacf70b44a8a0fd493018af4ef5afcff3
Philippe Fanaro <philippefanaro@gmail.com>
2025-07-30 11:00:01 +0900

    able to extract book problems in bulk now (but I think they are kind of throttling me now

--- src/scraping/single_problem.js
+++ src/scraping/single_problem.js
@@ -1,11 +1,8 @@
 const puppeteer = require("puppeteer")
 const fs = require("fs")
 
-const sampleProblemUrl = "https://www.101weiqi.com/q/17773/"
-const sampleBookUrl = "https://www.101weiqi.com/book/3/"
 const weiqi101login = "https://www.101weiqi.com/login/"
-
-const dummyAccount = "comoti7227@atebin.com"
+const dummyAccount = "comoti7227@atebin.com" // From [this project](https://github.com/101books/101books.github.io/blob/main/download.sh)
 
 let browser
 let page
@@ -37,24 +34,53 @@ function toSGFCoords(data) {
   return sgf
 }
 
-async function fetchProblem() {
-  await login()
-
+async function fetchProblem(problemId, path = "./problems/") {
   // Go to the Problem Page
-  await page.goto(sampleProblemUrl)
+  await page.goto(`https://www.101weiqi.com/q/${problemId}/`)
 
   // Getting the Data into an SGF
   const qqdata = await page.evaluate(() => window.qqdata)
   const sgfString = toSGFCoords(qqdata)
 
   // Save the SGF to a File
-  const filePath = `./problems/${qqdata.id}.sgf`
+  const filePath = `${path}${qqdata.id}.sgf`
   fs.writeFileSync(filePath, sgfString)
+}
+
+async function fetchBookProblems(bookId, levelOrder = false) {
+  if (levelOrder) await fetchBookProblemsByLevelOrder(bookId)
+}
+
+async function fetchBookProblemsByLevelOrder(bookId) {
+  const bookUrl = `https://www.101weiqi.com/book/levelorder/${bookId}`
+  await page.goto(bookUrl)
+
+  const initialPagedata = await page.evaluate(() => window.pagedata)
+
+  const pageTotal = Math.ceil(initialPagedata.qtotal / initialPagedata.qstep)
+
+  for (let i = 1; i < pageTotal; i++) {
+    const bookUrlWithPage = `${bookUrl}?page=${i}`
+    console.log(bookUrlWithPage)
+
+    await page.goto(bookUrlWithPage)
+
+    const pagedata = await page.evaluate(() => window.pagedata)
+    const questionIds = pagedata.qs.map((q) => q.qid)
+
+    for (const id of questionIds) {
+      await fetchProblem(id, `./books/${bookId}/`)
+    }
+  }
+}
+
+async function fetchData() {
+  await login()
+
+  // await fetchBookProblems(3, true)
+  await fetchProblem(17933)
 
   await browser.close()
 }
 
-// await function fetchBookProblems() {}
-
-login().then(() => fetchProblem())
-
+fetchData()
