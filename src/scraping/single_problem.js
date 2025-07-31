c0e173982c10b5cd3d99abcaa2f5c981c84d1afb
Philippe Fanaro <philippefanaro@gmail.com>
2025-07-31 16:11:31 +0900

    trying to have scraping work

--- src/scraping/single_problem.js
+++ src/scraping/single_problem.js
@@ -2,7 +2,19 @@ const puppeteer = require("puppeteer")
 const fs = require("fs")
 
 const weiqi101login = "https://www.101weiqi.com/login/"
-const dummyAccount = "comoti7227@atebin.com" // From [this project](https://github.com/101books/101books.github.io/blob/main/download.sh)
+// From [this project](https://github.com/101books/101books.github.io/blob/main/download.sh)
+const dummyAccounts = [
+  "comoti7227@atebin.com",
+  "fimowal358@apn7.com",
+  "mahet70277@bacaki.com",
+  "wojaga4524@calunia.com",
+  "sababib768@cartep.com",
+  "fetayi5707@furnato.com",
+  "rarejik433@apn7.com",
+  "riyodov302@alientex.com",
+  "perakax293@mvpalace.com",
+  "kowon74620@alientex.com",
+]
 
 let browser
 let page
@@ -13,8 +25,8 @@ async function login() {
 
   await page.goto(weiqi101login)
 
-  await page.type(`input[x-model="loginName"]`, dummyAccount)
-  await page.type(`input[x-model="loginPassword"]`, dummyAccount)
+  await page.type(`input[x-model="loginName"]`, dummyAccounts[1])
+  await page.type(`input[x-model="loginPassword"]`, dummyAccounts[1])
 
   await page.click(`.login-button.active`)
 
@@ -34,7 +46,7 @@ function toSGFCoords(data) {
   return sgf
 }
 
-async function fetchProblem(problemId, path = "./problems/") {
+async function fetchProblem(problemId, path = "./problems/", filename = "") {
   // Go to the Problem Page
   await page.goto(`https://www.101weiqi.com/q/${problemId}/`)
 
@@ -43,7 +55,8 @@ async function fetchProblem(problemId, path = "./problems/") {
   const sgfString = toSGFCoords(qqdata)
 
   // Save the SGF to a File
-  const filePath = `${path}${qqdata.id}.sgf`
+  const name = filename === "" ? qqdata.id : filename
+  const filePath = `${path}${name}.sgf`
   fs.writeFileSync(filePath, sgfString)
 }
 
@@ -68,8 +81,16 @@ async function fetchBookProblemsByLevelOrder(bookId) {
     const pagedata = await page.evaluate(() => window.pagedata)
     const questionIds = pagedata.qs.map((q) => q.qid)
 
+    let count = 1
     for (const id of questionIds) {
-      await fetchProblem(id, `./books/${bookId}/`)
+      setTimeout(async () => {
+        await fetchProblem(
+          id,
+          `../sgf/101weiqi_books/${bookId}/`,
+          count.toString()
+        )
+      }, 1_000) // Wait a bit to avoid overwhelming the server
+      count++
     }
   }
 }
@@ -77,8 +98,8 @@ async function fetchBookProblemsByLevelOrder(bookId) {
 async function fetchData() {
   await login()
 
-  // await fetchBookProblems(3, true)
-  await fetchProblem(17933)
+  await fetchBookProblems(3, true)
+  // await fetchProblem(17933)
 
   await browser.close()
 }
